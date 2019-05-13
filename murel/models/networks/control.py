ea1931f4aa008a7399628d70b4a02e1046efac12
gokcengokceoglu <gokcengokceoglu.95@gmail.com>
2019-05-13 21:26:27 +0300

    lost scripts found

---
+++ murel/models/networks/control.py
@@ -0,0 +1,27 @@
+import torch
+import numpy as np
+
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+import torch.nn as nn
+import torch.nn.functional as F
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class ControlModule(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(ControlModule, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
--- murel/models/networks/itereg_cell.py
+++ murel/models/networks/itereg_cell.py
@@ -6,6 +6,8 @@ import torch.nn.functional as F
 import block
 from .pairwise import Pairwise
 from .graph import GraphModule
+from .control import ControlModule
+from .relations import RelationsModule
 
 class iteregCell(nn.Module):
     def __init__(self,
@@ -21,35 +23,37 @@ class iteregCell(nn.Module):
 
         self.fuse = block.factory_fusion(self.fusion)
         self.graph_module = GraphModule(**graph)
-        self.control_module =ControlModule(**control)
+        self.control_module = ControlModule(**control)
         self.relations_module = RelationsModule(**relations)
 
 
-    def forward(self, q, s, coords=None):
+    def forward(self, q, s_i, c_i, coords=None):
 
         # 1. fuse q and s_(i-1) find m_i
-        m_i = self.fusion_module(q, s)
+        m_new = self.fusion_module(q, s_i)
 
         # 2. put q and c_(i-1) to recurrent unit, find c_i
-        c_i = self.control_module(q)
+        c_new = self.control_module(q, c_i)
 
         # 3. put c_i and m_i to the graph module => g_i = M*M'*c_i
 
         # graph learner
-        g_i = self.graph_module(in_feature_dim=feat_dim + hid_dim,
+        feat_dim = m_new.shape[1]
+        hid_dim = c_new.shape[0]
+        g_new = self.graph_module(in_feature_dim=feat_dim + hid_dim,
                                         combined_feature_dim=512,
                                         K=36,
-                                        dropout=dropout)
+                                        dropout=1)
 
 
         # 4. put m_i and g_i to relations module =>
             # B(b_i,b_j,W) = r_ij, e_i = sum over j(gi*r_ij)
             # s_i = s_(i-1) + e_i
-        e_i = self.relations_module(g_i, m_i)
+        e_i = self.relations_module(g_new, m_new)
 
-        s_new = s + e_i
+        s_new = s_i + e_i
 
-        return s_new, c_i
+        return s_new, c_new
 
 
 
